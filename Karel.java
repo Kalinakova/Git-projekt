602d066b894dd4b211b4dee88136f16e327ecb94
Kalinakova <marketakalinakova@gmail.com>
2025-05-09 22:12:48 +0200

    change file name

---
+++ Karel.java
@@ -0,0 +1,12 @@
+class Karel {
+    public static void main (String arg []) {
+	String jmeno = "Karel Novak";
+	char b = 'K';
+	char c = 'a';
+	char d = 'r';
+	char e = 'e';
+	char f = 'l';
+	double a = b + c + d + f;
+	    System.out. println ("Kod unicode je"  +a);
+    }
+}
